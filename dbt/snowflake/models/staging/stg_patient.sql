126ff5a3d206165f165d0414ef1324321681893c
Rob Ford <rob@defact.io>
2025-12-15 15:18:02 -0800

    feat: Add dbt staging model for patient data, including its source definition and schema tests.

---
+++ dbt/snowflake/models/staging/stg_patient.sql
@@ -0,0 +1,35 @@
+{{ config(materialized='view') }}
+
+with source as (
+    select * from {{ source('raw', 'patients') }}
+),
+
+renamed as (
+    select
+        -- Extract ID
+        record_content:id::string as patient_id,
+        
+        -- Core Demographics
+        record_content:active::boolean as active,
+        record_content:gender::string as gender,
+        record_content:birthDate::date as birth_date,
+        record_content:deceasedDateTime::timestamp as deceased_datetime,
+        
+        -- Name (First element of array)
+        record_content:name[0].family::string as name_family,
+        record_content:name[0].given[0]::string as name_given,
+        
+        -- Address (First element)
+        record_content:address[0].city::string as city,
+        record_content:address[0].state::string as state,
+        record_content:address[0].postalCode::string as postal_code,
+        record_content:address[0].country::string as country,
+        
+        -- Metadata
+        source_file,
+        ingestion_time
+        
+    from source
+)
+
+select * from renamed
